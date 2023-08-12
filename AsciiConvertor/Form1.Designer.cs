f29ed11bfd40a0d40a026f29b55ecb1ffaf9fea0
Imesh <imeshisuranga00@gmail.com>
2023-08-13 03:43:58 +0530

    Added new label to interface and added function for btn

--- AsciiConvertor/Form1.Designer.cs
+++ AsciiConvertor/Form1.Designer.cs
@@ -29,14 +29,15 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtOne = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtTwo = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtThree = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.lblOne = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,13 +50,14 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Gcode Syntax for your machine IE:M97,H98,F15";
             // 
-            // textBox1
+            // txtOne
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(17, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(449, 28);
-            this.textBox1.TabIndex = 1;
+            this.txtOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOne.Location = new System.Drawing.Point(17, 54);
+            this.txtOne.Name = "txtOne";
+            this.txtOne.Size = new System.Drawing.Size(449, 28);
+            this.txtOne.TabIndex = 1;
+            this.txtOne.TextChanged += new System.EventHandler(this.txtOneChange);
             // 
             // checkBox1
             // 
@@ -71,29 +73,30 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Location = new System.Drawing.Point(12, 169);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(254, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Enter text to be engraved";
             // 
-            // textBox2
+            // txtTwo
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(17, 199);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(449, 28);
-            this.textBox2.TabIndex = 4;
+            this.txtTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTwo.Location = new System.Drawing.Point(17, 228);
+            this.txtTwo.Name = "txtTwo";
+            this.txtTwo.Size = new System.Drawing.Size(449, 28);
+            this.txtTwo.TabIndex = 4;
             // 
-            // button1
+            // btnGenerate
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(122, 274);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 38);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Generate Code";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(122, 288);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(176, 38);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate Code";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
             // label3
             // 
@@ -105,13 +108,14 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Ascii to Gcode Output";
             // 
-            // richTextBox1
+            // txtThree
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(47, 406);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(354, 277);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
+            this.txtThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThree.Location = new System.Drawing.Point(47, 406);
+            this.txtThree.Name = "txtThree";
+            this.txtThree.Size = new System.Drawing.Size(354, 277);
+            this.txtThree.TabIndex = 7;
+            this.txtThree.Text = "";
             // 
             // button2
             // 
@@ -123,19 +127,30 @@
             this.button2.Text = "Save to Text File";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // lblOne
+            // 
+            this.lblOne.AutoSize = true;
+            this.lblOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOne.Location = new System.Drawing.Point(163, 112);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(58, 22);
+            this.lblOne.TabIndex = 10;
+            this.lblOne.Text = "label5";
+            // 
             // AsciiCovertor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(701, 753);
+            this.Controls.Add(this.lblOne);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtThree);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtTwo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtOne);
             this.Controls.Add(this.label1);
             this.Name = "AsciiCovertor";
             this.Text = "AsciiConvertor";
@@ -148,14 +163,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtOne;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTwo;
+        private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox txtThree;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblOne;
     }
 }
 
--- AsciiConvertor/Form1.cs
+++ AsciiConvertor/Form1.cs
@@ -15,11 +15,29 @@ namespace AsciiConvertor
         public AsciiCovertor()
         {
             InitializeComponent();
+            lblOne.Text = txtOne.Text.Trim();
         }
 
         private void AsciiCovertor_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtOneChange(object sender, EventArgs e)
+        {
+            lblOne.Text = txtOne.Text;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+            // Convert the string to ASCII
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(txtTwo.Text);
+            foreach (byte b in asciiBytes)
+            {
+                txt += lblOne.Text + "  p" +($"{b}")+Environment.NewLine;
+            }
+            txtThree.Text = txt;
         }
     }
 }
